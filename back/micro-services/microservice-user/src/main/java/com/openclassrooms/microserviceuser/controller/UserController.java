1e70c36fb4e88d395613b8af188c1b031fa3a74d
romain-25 <romain.rouabah@gmail.com>
2024-07-25 17:05:27 +0200

    login ok with posman , if connetion true , return token else return message

--- back/micro-services/microservice-user/src/main/java/com/openclassrooms/microserviceuser/controller/UserController.java
+++ back/micro-services/microservice-user/src/main/java/com/openclassrooms/microserviceuser/controller/UserController.java
@@ -4,6 +4,7 @@ import com.openclassrooms.microserviceuser.dto.UserLoginDto;
 import com.openclassrooms.microserviceuser.model.UserModel;
 import com.openclassrooms.microserviceuser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,12 +16,12 @@ public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("/auth/login")
-    public UserLoginDto login(@RequestBody UserLoginDto user) {
-//       UserModel foundAccount =
+    public ResponseEntity<?> login(@RequestBody UserLoginDto userLogin) {
+       return userService.checkLogin(userLogin);
     }
 
-    @GetMapping("/profil")
+    @GetMapping("/auth/profil")
     public UserModel profil(){
-        return userService.findByEmail("romain@test.fr");
+        return userService.getUserByEmail("romain@test.fr");
     }
 }
--- back/micro-services/microservice-user/src/main/java/com/openclassrooms/microserviceuser/service/UserService.java
+++ back/micro-services/microservice-user/src/main/java/com/openclassrooms/microserviceuser/service/UserService.java
@@ -1,16 +1,77 @@
 package com.openclassrooms.microserviceuser.service;
 
+import com.openclassrooms.microserviceuser.dto.UserLoginDto;
+import com.openclassrooms.microserviceuser.dto.UserRegisterDto;
 import com.openclassrooms.microserviceuser.model.UserModel;
 import com.openclassrooms.microserviceuser.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
-
-    public UserModel findByEmail(String email) {
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtService jwtService;
+    /**
+     * Retrieves a user by their email.
+     *
+     * @param email the email of the user to retrieve
+     * @return the user model
+     */
+    public UserModel getUserByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+    /**
+     * Creates a new user.
+     *
+     * @param userDto the DTO containing the user details
+     * @return the created user model
+     */
+    public UserModel registerUser(UserRegisterDto userDto) {
+        UserModel user = modelMapper.map(userDto, UserModel.class);
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        return userRepository.save(user);
+    }
+    /**
+     * Checks if the provided password matches the stored password for the user.
+     *
+     * @param userLoginDto the DTO containing the login details
+     * @param user         the user model
+     * @return true if the passwords match, false otherwise
+     */
+    public boolean checkPassword(UserLoginDto userLoginDto, UserModel user) {
+        return passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword());
+    }
+
+    public ResponseEntity<?> checkLogin(UserLoginDto userLoginDto) {
+        UserModel foundUser = userRepository.findByEmail(userLoginDto.getEmail());
+        if(foundUser != null) {
+            boolean bcheckPassword = checkPassword(userLoginDto, foundUser);
+            if(bcheckPassword) {
+                Authentication authentication = new UsernamePasswordAuthenticationToken(foundUser.getEmail(), null, new ArrayList<>());
+                String token = jwtService.generateToken(authentication);
+                return ResponseEntity.ok(token);
+            }else{
+                // Mot de pass incorrect
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email ou mot de passe incorrect");
+            }
+        }else {
+            // Email incorrect
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email ou mot de passe incorrect");
+        }
     }
 }
