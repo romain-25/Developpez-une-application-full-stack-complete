f231e2e003e0ba5e2ed7f0298d012ed343701aa1
romain-25 <53046144+romain-25@users.noreply.github.com>
2024-09-05 23:12:20 +0200

    Update MDD_project.sql

--- MDD_project.sql
+++ MDD_project.sql
@@ -1,61 +1,84 @@
-
--- Create table for Users
-CREATE TABLE IF NOT EXISTS users (
+-- Create table for User
+CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
--- Create table for Themes
-CREATE TABLE IF NOT EXISTS themes (
+-- Create table for Theme
+CREATE TABLE IF NOT EXISTS theme (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     content TEXT NOT NULL
 );
 
--- Create table for Articles
-CREATE TABLE IF NOT EXISTS articles (
+-- Create table for Article
+CREATE TABLE IF NOT EXISTS article (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     theme_id INT,
+    author_id INT,
+    published_date date
 );
-
+-- Create table for Comment
+CREATE TABLE IF NOT EXISTS comment (
+id INT AUTO_INCREMENT PRIMARY KEY,
+content TEXT NOT NULL,
+author_id INT,
+article_id INT,
+created_at date
+);
 -- Create table for Subscriptions (many-to-many relationship between users and themes)
-CREATE TABLE IF NOT EXISTS subscriptions (
+CREATE TABLE IF NOT EXISTS user_theme_subscription (
     user_id INT,
     theme_id INT,
-    PRIMARY KEY (user_id, theme_id),
+    PRIMARY KEY (user_id, theme_id)
 );
 
 -- Insert the user with id 1
-INSERT INTO users (id, username, email, password) VALUES
-(1, 'romain', 'romain@mdd.fr', '$2a$10$st5IWlEks.oq6avL//StxeJwGNf/BPPPeiEnR36Hz2KWM9KXW8tPu');
+INSERT INTO user (id, username, email, password) VALUES
+(1, 'romain', 'romain@mdd.fr', '$2b$12$dJwZlfkrOJXaAWRXPNXMP.n2GcdZY9KgEJT.TIbu7RoYq/ciahG4q');
 
 -- Insert themes
-INSERT INTO themes (name, content) VALUES
+INSERT INTO theme (name, content) VALUES
 ('Java', 'Lorem ipsum dolor sit amet, Java theme content.'),
 ('Python', 'Lorem ipsum dolor sit amet, Python theme content.'),
 ('Angular', 'Lorem ipsum dolor sit amet, Angular theme content.'),
 ('C sharp', 'Lorem ipsum dolor sit amet, C sharp theme content.');
 
 -- Insert articles for each theme
-INSERT INTO articles (title, content, theme_id) VALUES
-('Java Article 1', 'Lorem ipsum dolor sit amet, article content 1.', (SELECT id FROM themes WHERE name = 'Java')),
-('Java Article 2', 'Lorem ipsum dolor sit amet, article content 2.', (SELECT id FROM themes WHERE name = 'Java')),
-('Java Article 3', 'Lorem ipsum dolor sit amet, article content 3.', (SELECT id FROM themes WHERE name = 'Java')),
-('Python Article 1', 'Lorem ipsum dolor sit amet, article content 1.', (SELECT id FROM themes WHERE name = 'Python')),
-('Python Article 2', 'Lorem ipsum dolor sit amet, article content 2.', (SELECT id FROM themes WHERE name = 'Python')),
-('Python Article 3', 'Lorem ipsum dolor sit amet, article content 3.', (SELECT id FROM themes WHERE name = 'Python')),
-('Angular Article 1', 'Lorem ipsum dolor sit amet, article content 1.', (SELECT id FROM themes WHERE name = 'Angular')),
-('Angular Article 2', 'Lorem ipsum dolor sit amet, article content 2.', (SELECT id FROM themes WHERE name = 'Angular')),
-('Angular Article 3', 'Lorem ipsum dolor sit amet, article content 3.', (SELECT id FROM themes WHERE name = 'Angular')),
-('C sharp Article 1', 'Lorem ipsum dolor sit amet, article content 1.', (SELECT id FROM themes WHERE name = 'C sharp')),
-('C sharp Article 2', 'Lorem ipsum dolor sit amet, article content 2.', (SELECT id FROM themes WHERE name = 'C sharp')),
-('C sharp Article 3', 'Lorem ipsum dolor sit amet, article content 3.', (SELECT id FROM themes WHERE name = 'C sharp'));
+INSERT INTO article (title, content, theme_id, author_id, published_date) VALUES
+('Java Article 1', 'Lorem ipsum dolor sit amet, article content 1.', (SELECT id FROM theme WHERE name = 'Java'),1, CURDATE()),
+('Java Article 2', 'Lorem ipsum dolor sit amet, article content 2.', (SELECT id FROM theme WHERE name = 'Java'),1, CURDATE()),
+('Java Article 3', 'Lorem ipsum dolor sit amet, article content 3.', (SELECT id FROM theme WHERE name = 'Java'),1, CURDATE()),
+('Python Article 1', 'Lorem ipsum dolor sit amet, article content 1.', (SELECT id FROM theme WHERE name = 'Python'),1, CURDATE()),
+('Python Article 2', 'Lorem ipsum dolor sit amet, article content 2.', (SELECT id FROM theme WHERE name = 'Python'),1, CURDATE()),
+('Python Article 3', 'Lorem ipsum dolor sit amet, article content 3.', (SELECT id FROM theme WHERE name = 'Python'),1, CURDATE()),
+('Angular Article 1', 'Lorem ipsum dolor sit amet, article content 1.', (SELECT id FROM theme WHERE name = 'Angular'),1, CURDATE()),
+('Angular Article 2', 'Lorem ipsum dolor sit amet, article content 2.', (SELECT id FROM theme WHERE name = 'Angular'),1, CURDATE()),
+('Angular Article 3', 'Lorem ipsum dolor sit amet, article content 3.', (SELECT id FROM theme WHERE name = 'Angular'),1, CURDATE()),
+('C sharp Article 1', 'Lorem ipsum dolor sit amet, article content 1.', (SELECT id FROM theme WHERE name = 'C sharp'),1, CURDATE()),
+('C sharp Article 2', 'Lorem ipsum dolor sit amet, article content 2.', (SELECT id FROM theme WHERE name = 'C sharp'),1, CURDATE()),
+('C sharp Article 3', 'Lorem ipsum dolor sit amet, article content 3.', (SELECT id FROM theme WHERE name = 'C sharp'),1, CURDATE());
+
+-- Insert comment in articles
+INSERT INTO comment (content, author_id, article_id, created_at) VALUES
+('Lorem ipsum dolor sit amet, comment content 1.', 1,1, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,2, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,3, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,4, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,5, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,6, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,7, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,8, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,9, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,10, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,11, CURDATE()),
+('Lorem ipsum dolor sit amet, comment content 1.', 1,12, CURDATE());
 
 -- Subscribe the user with id 1 to the Java and Angular themes
-INSERT INTO subscriptions (user_id, theme_id) VALUES
-(1, (SELECT id FROM themes WHERE name = 'Java')),
-(1, (SELECT id FROM themes WHERE name = 'Angular'));
+INSERT INTO user_theme_subscription (user_id, theme_id) VALUES
+(1, (SELECT id FROM theme WHERE name = 'Java')),
+(1, (SELECT id FROM theme WHERE name = 'Angular'));
